0ee0580ef69d249c00477b3bcb4eaed92b27e035
Ryan-Riaz <devryanriaz.gmail.com>
2021-01-08 12:27:28 +0600

    initial project setup done

---
+++ src/Currency.jsx
@@ -0,0 +1,12 @@
+const Currency = () => {
+    return (
+        <div>
+            <input type="number" className="input" />
+            <select>
+                <option value="hi">hi</option>
+            </select>
+        </div>
+    );
+};
+
+export default Currency;
