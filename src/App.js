3fdc1d74f07d06f93b862606a1c4cab645c9adab
Ryan-Riaz <devryanriaz.gmail.com>
2021-01-10 02:30:01 +0600

    input functions added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
     const [currency, setCurrency] = useState([]);
     const [fromCurrency, setFromCurrency] = useState();
     const [toCurrency, setToCurrency] = useState();
+    const [exchange, setExchange] = useState();
+    const [amount, setAmount] = useState(1);
+    const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+
     useEffect(() => {
         fetch(BASE_URL)
             .then((res) => res.json())
@@ -16,8 +20,29 @@ function App() {
                 setCurrency([data.base, ...Object.keys(data.rates)]);
                 setFromCurrency(data.base);
                 setToCurrency(firstCurrency);
+                setExchange(data.rates[firstCurrency]);
             });
     }, []);
+
+    let fromAmount, toAmount;
+    if (amountInFromCurrency) {
+        fromAmount = amount;
+        toAmount = fromAmount * exchange;
+    } else {
+        toAmount = amount;
+        fromAmount = toAmount / exchange;
+    }
+
+    function handleFromChangeAmount(e) {
+        setAmount(e.target.value);
+        setAmountInFromCurrency(true);
+    }
+
+    function handleToChangeAmount(e) {
+        setAmount(e.target.value);
+        setAmountInFromCurrency(false);
+    }
+
     return (
         <>
             <h1>Currency Converter</h1>
@@ -25,12 +50,16 @@ function App() {
                 currency={currency}
                 selectedCurrency={fromCurrency}
                 onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+                amount={fromAmount}
+                onChangeAmount={handleFromChangeAmount}
             />
             <div className="equal">=</div>
             <Currency
                 currency={currency}
                 selectedCurrency={toCurrency}
                 onChangeCurrency={(e) => setToCurrency(e.target.value)}
+                amount={toAmount}
+                onChangeAmount={handleToChangeAmount}
             />
         </>
     );
--- src/Currency.jsx
+++ src/Currency.jsx
@@ -1,8 +1,19 @@
 import { v4 as uuid } from "uuid";
-const Currency = ({ currency, selectedCurrency, onChangeCurrency }) => {
+const Currency = ({
+    currency,
+    selectedCurrency,
+    onChangeCurrency,
+    amount,
+    onChangeAmount,
+}) => {
     return (
         <div>
-            <input type="number" className="input" />
+            <input
+                type="number"
+                className="input"
+                value={amount.toString()}
+                onChange={onChangeAmount}
+            />
             <select value={selectedCurrency} onChange={onChangeCurrency}>
                 {currency.map((option) => (
                     <option key={uuid()} value={option}>
