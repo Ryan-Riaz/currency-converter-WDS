9f204cced671e6efb7a58420a0191bd0cceeb826
Ryan-Riaz <devryanriaz.gmail.com>
2021-01-10 01:49:28 +0600

    select onchange function added

--- src/App.js
+++ src/App.js
@@ -6,20 +6,32 @@ const BASE_URL = "https://api.exchangeratesapi.io/latest";
 
 function App() {
     const [currency, setCurrency] = useState([]);
+    const [fromCurrency, setFromCurrency] = useState();
+    const [toCurrency, setToCurrency] = useState();
     useEffect(() => {
         fetch(BASE_URL)
             .then((res) => res.json())
             .then((data) => {
+                const firstCurrency = Object.keys(data.rates)[0];
                 setCurrency([data.base, ...Object.keys(data.rates)]);
-                console.log(data);
+                setFromCurrency(data.base);
+                setToCurrency(firstCurrency);
             });
     }, []);
     return (
         <>
             <h1>Currency Converter</h1>
-            <Currency currency={currency} />
+            <Currency
+                currency={currency}
+                selectedCurrency={fromCurrency}
+                onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+            />
             <div className="equal">=</div>
-            <Currency currency={currency} />
+            <Currency
+                currency={currency}
+                selectedCurrency={toCurrency}
+                onChangeCurrency={(e) => setToCurrency(e.target.value)}
+            />
         </>
     );
 }
--- src/Currency.jsx
+++ src/Currency.jsx
@@ -1,9 +1,9 @@
 import { v4 as uuid } from "uuid";
-const Currency = ({ currency }) => {
+const Currency = ({ currency, selectedCurrency, onChangeCurrency }) => {
     return (
         <div>
             <input type="number" className="input" />
-            <select>
+            <select value={selectedCurrency} onChange={onChangeCurrency}>
                 {currency.map((option) => (
                     <option key={uuid()} value={option}>
                         {option}
